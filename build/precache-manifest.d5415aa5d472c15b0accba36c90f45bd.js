69e4c0bdb1b65e0c6f9de67e820827e16fa9b8bc
YASH PRIYAM <yashpriyam@YASHs-MacBook-Air.local>
2020-04-12 00:59:16 +0530

    final commmit

---
+++ build/precache-manifest.d5415aa5d472c15b0accba36c90f45bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cfd8adf4656c7fe86245b910cb41682",
+    "url": "/red2_proj/index.html"
+  },
+  {
+    "revision": "a5419ce2d3508aa1f12d",
+    "url": "/red2_proj/static/css/main.f2c8ad92.chunk.css"
+  },
+  {
+    "revision": "94249045a14b8749d2de",
+    "url": "/red2_proj/static/js/2.a52eccb6.chunk.js"
+  },
+  {
+    "revision": "a5419ce2d3508aa1f12d",
+    "url": "/red2_proj/static/js/main.d4d66502.chunk.js"
+  },
+  {
+    "revision": "b963e6fc6950620d7dd4",
+    "url": "/red2_proj/static/js/runtime~main.5af86c05.js"
+  }
+]);
