674f06b6102476c7a8f434ec3b7105dcba370847
YASH PRIYAM <yashpriyam@YASHs-MacBook-Air.local>
2020-04-12 01:20:31 +0530

    Updates

--- precache-manifest.64726d226157773948228ea95e2c20df.js
+++ precache-manifest.64726d226157773948228ea95e2c20df.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4cfd8adf4656c7fe86245b910cb41682",
+    "revision": "44e84405f33bba6049ea258ac2a6c786",
     "url": "/red2_proj/index.html"
   },
   {
-    "revision": "a5419ce2d3508aa1f12d",
+    "revision": "01adddbf4b3c19cb02bb",
     "url": "/red2_proj/static/css/main.f2c8ad92.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/red2_proj/static/js/2.a52eccb6.chunk.js"
   },
   {
-    "revision": "a5419ce2d3508aa1f12d",
-    "url": "/red2_proj/static/js/main.d4d66502.chunk.js"
+    "revision": "01adddbf4b3c19cb02bb",
+    "url": "/red2_proj/static/js/main.75ea3e1e.chunk.js"
   },
   {
     "revision": "b963e6fc6950620d7dd4",
